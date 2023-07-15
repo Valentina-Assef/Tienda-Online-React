6191df7da32ab4c0c5a4e50332cb2d0d59e7eb99
Valentina-Assef <valenassef@gmail.com>
2023-07-15 00:18:53 -0300

    e-commerce

--- tienda-online/src/App.js
+++ tienda-online/src/App.js
@@ -4,12 +4,14 @@ import "boxicons";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Paginas } from "./componentes/Paginas";
 import { DataProvider } from "./context/Dataprovider";
+import { Carrito } from "./componentes/Carrito";
 
 function App() {
   return (
     <DataProvider>
       <Router>
         <Header />
+        <Carrito />
         <Paginas />
       </Router>
     </DataProvider>
